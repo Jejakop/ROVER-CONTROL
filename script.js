4fa7588e103bf3acbffff58a36d9c14fe10c06f9
Jejakop <95611269+Jejakop@users.noreply.github.com>
2025-12-10 21:25:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const esp32IP = "http://192.168.0.73"; // <- wpisz IP Twojego ESP32
+const esp32IP = "http://192.168.0.73"; // Twój ESP32 IP
 
 function sendCommand(cmd) {
   fetch(`${esp32IP}/${cmd}`)
     .then(response => response.text())
-    .then(text => alert(text))
-    .catch(err => alert("Błąd połączenia z ESP32"));
+    .then(text => {
+      console.log("Odpowiedź ESP32:", text);
+      // Możesz dodać wyświetlanie na stronie zamiast alertu
+    })
+    .catch(err => {
+      console.error("Błąd połączenia z ESP32:", err);
+      // Możesz pokazać komunikat na stronie
+    });
 }
 
 function setSpeed(val) {
   document.getElementById("speedValue").innerText = val;
   fetch(`${esp32IP}/speed?val=${val}`)
-    .catch(err => alert("Błąd połączenia z ESP32"));
+    .catch(err => {
+      console.error("Błąd połączenia z ESP32:", err);
+    });
 }
-
