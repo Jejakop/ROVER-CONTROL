067554a4c090f8e59389d3f62d455b975bba223a
Jejakop <95611269+Jejakop@users.noreply.github.com>
2025-12-10 21:16:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const esp32IP = "http://192.168.1.123"; // <- wpisz IP Twojego ESP32
+
+function sendCommand(cmd) {
+  fetch(`${esp32IP}/${cmd}`)
+    .then(response => response.text())
+    .then(text => alert(text))
+    .catch(err => alert("Błąd połączenia z ESP32"));
+}
+
+function setSpeed(val) {
+  document.getElementById("speedValue").innerText = val;
+  fetch(`${esp32IP}/speed?val=${val}`)
+    .catch(err => alert("Błąd połączenia z ESP32"));
+}
